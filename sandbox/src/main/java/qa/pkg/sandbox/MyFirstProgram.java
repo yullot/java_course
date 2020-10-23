0f01156f6a0a9eec8e88343c37dfafe1b74cda81
yullot <yulok1102@mail.ru>
2020-10-24 00:00:37 +0300

    changed code format settings

--- sandbox/src/main/java/qa/pkg/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/qa/pkg/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package qa.pkg.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
