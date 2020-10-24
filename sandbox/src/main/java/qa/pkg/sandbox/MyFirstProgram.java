33d71d6ea59c2f2d797610d419e0c4afecc9861a
yullot <yulok1102@mail.ru>
2020-10-24 23:01:48 +0300

    Task2: get distance between 2 points

--- sandbox/src/main/java/qa/pkg/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/qa/pkg/sandbox/MyFirstProgram.java
@@ -2,6 +2,9 @@ package qa.pkg.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Point pointA = new Point(5, 6);
+    Point pointB = new Point(0, 8);
+    System.out.println("Distance between point A (" + pointA.x + ", " + pointA.y +
+            ") and point B (" + pointA.x + ", " + pointA.y + ") equals to " + pointA.getDistance(pointB));
   }
 }
---
+++ sandbox/src/main/java/qa/pkg/sandbox/Point.java
@@ -0,0 +1,18 @@
+package qa.pkg.sandbox;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+  
+  public double getDistance(Point p2) {
+    return sqrt(pow((this.x - p2.x), 2) + pow((this.y - p2.y), 2));
+  }
+}
