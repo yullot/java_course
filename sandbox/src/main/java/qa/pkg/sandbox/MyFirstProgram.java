1468591a22c3a499ce4a699655c18bfb67aa7b95
yullot <yulok1102@mail.ru>
2020-10-24 23:23:11 +0300

    Correct output of cordinates for point B

--- sandbox/src/main/java/qa/pkg/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/qa/pkg/sandbox/MyFirstProgram.java
@@ -4,7 +4,7 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     Point pointA = new Point(5, 6);
     Point pointB = new Point(0, 8);
-    System.out.println("Distance between point A (" + pointA.x + ", " + pointA.y +
-            ") and point B (" + pointA.x + ", " + pointA.y + ") equals to " + pointA.getDistance(pointB));
+    System.out.println("The distance between a point A (" + pointA.x + ", " + pointA.y +
+            ") and a point B (" + pointB.x + ", " + pointB.y + ") equals to " + pointA.getDistance(pointB));
   }
 }
--- sandbox/src/main/java/qa/pkg/sandbox/Point.java
+++ sandbox/src/main/java/qa/pkg/sandbox/Point.java
@@ -11,7 +11,7 @@ public class Point {
     this.x = x;
     this.y = y;
   }
-  
+
   public double getDistance(Point p2) {
     return sqrt(pow((this.x - p2.x), 2) + pow((this.y - p2.y), 2));
   }
