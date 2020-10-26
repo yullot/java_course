bf8f32be9e46788fd6eede3433b75a4f7bb8d307
yullot <yulok1102@mail.ru>
2020-10-26 21:10:16 +0300

    Task3: Tests for class Point

---
+++ sandbox/src/test/java/qa/pkg/sandbox/PointTests.java
@@ -0,0 +1,32 @@
+package qa.pkg.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+  @Test
+  public void testDistanceZeroPoint() {
+    Point pointC = new Point(0, 0);
+    Point pointB = new Point(-5, -8);
+    Assert.assertEquals(pointC.getDistance(pointB),9.4339,0.0001);
+  }
+  @Test
+  public void testDistanceForOnePoint() {
+    Point pointC = new Point(6, 120);
+    Point pointB = new Point(6, 120);
+    Assert.assertEquals(pointC.getDistance(pointB),0);
+  }
+  @Test
+  public void testDistanceForNegativeAndPositiveCoord() {
+    Point pointC = new Point(-6, 120);
+    Point pointB = new Point(6, -120);
+    Assert.assertEquals(pointC.getDistance(pointB),240.2998,0.0001);
+  }
+
+  @Test
+  public void testDistanceIntegerAnswer() {
+    Point pointC = new Point(1, 5);
+    Point pointB = new Point(4, 1);
+    Assert.assertEquals(pointC.getDistance(pointB),5);
+  }
+}
