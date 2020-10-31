0a080bf3b701e35cc49936d51944c6984cbe1e6b
yullot <yulok1102@mail.ru>
2020-10-31 16:23:34 +0300

    changes in structure

--- sandbox/src/test/java/qa/pkg/sandbox/PointTests.java
+++ sandbox/src/test/java/qa/pkg/sandbox/PointTests.java
@@ -8,25 +8,27 @@ public class PointTests {
   public void testDistanceZeroPoint() {
     Point pointC = new Point(0, 0);
     Point pointB = new Point(-5, -8);
-    Assert.assertEquals(pointC.getDistance(pointB),9.4339,0.0001);
+    Assert.assertEquals(pointC.getDistance(pointB), 9.4339, 0.0001);
   }
+
   @Test
   public void testDistanceForOnePoint() {
     Point pointC = new Point(6, 120);
     Point pointB = new Point(6, 120);
-    Assert.assertEquals(pointC.getDistance(pointB),0);
+    Assert.assertEquals(pointC.getDistance(pointB), 0);
   }
+
   @Test
   public void testDistanceForNegativeAndPositiveCoord() {
     Point pointC = new Point(-6, 120);
     Point pointB = new Point(6, -120);
-    Assert.assertEquals(pointC.getDistance(pointB),240.2998,0.0001);
+    Assert.assertEquals(pointC.getDistance(pointB), 240.2998, 0.0001);
   }
 
   @Test
   public void testDistanceIntegerAnswer() {
     Point pointC = new Point(1, 5);
     Point pointB = new Point(4, 1);
-    Assert.assertEquals(pointC.getDistance(pointB),5);
+    Assert.assertEquals(pointC.getDistance(pointB), 5);
   }
 }
