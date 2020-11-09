a1e74227fc0944ab3f2f4dcaa2344e69ea8b8921
yullot <yulok1102@mail.ru>
2020-11-09 21:52:51 +0300

    added launching via Chrome browser

--- addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/ApplicationManager.java
@@ -1,27 +1,36 @@
 package qa.pkg.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   WebDriver wd;
+  String browser;
 
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
-  private SessionHelper sessionHelper;
   private ContactHelper contactHelper;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
 
   public void init() {
-    //System.setProperty("webdriver.gecko.driver","c:/Drivers/geckodriver.exe");
-    wd = new FirefoxDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
+    SessionHelper sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
     sessionHelper.login("admin", "secret");
   }
