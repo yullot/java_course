3be09c7067001c309a5392cacd5eff7dadac89f0
yullot <yulok1102@mail.ru>
2020-11-10 23:41:21 +0300

    added checks if already pages are opened
     goToGroupsPage()
     goToAddNewPage()
     goToHomePage()

--- addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/NavigationHelper.java
@@ -3,6 +3,8 @@ package qa.pkg.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.sql.SQLOutput;
+
 public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
@@ -10,17 +12,31 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupsPage() {
+    if (isElementPresent(By.cssSelector("div#content>h1"))
+            && wd.findElement(By.xpath("//div[@id='content']/h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void goToAddNewPage() {
+    if (isElementPresent(By.cssSelector("div#content>h1")) &&
+            wd.findElement(By.cssSelector("div#content>h1")).getText().equals("Edit / add address book entry")
+            && isElementPresent(By.name("submit"))) {
+      return;
+    }
     click(By.linkText("add new"));
   }
 
   public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
-  public  void confirmAlert(){
+
+  public void confirmAlert() {
     wd.switchTo().alert().accept();
   }
 }
