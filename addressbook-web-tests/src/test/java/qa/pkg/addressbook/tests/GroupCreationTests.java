2b12073111a0d6f1b417dea119426481341cef51
yullot <yulok1102@mail.ru>
2020-11-17 22:00:57 +0300

    add finding max using lambda-expressions

--- addressbook-web-tests/src/test/java/qa/pkg/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/qa/pkg/addressbook/tests/GroupCreationTests.java
@@ -1,10 +1,10 @@
 package qa.pkg.addressbook.tests;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import qa.pkg.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,13 +18,9 @@ public class GroupCreationTests extends TestBase {
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+    //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
   }
