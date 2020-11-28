09bee428e09ffe039047278ae59b796128e7a25a
yullot <yulok1102@mail.ru>
2020-11-28 20:51:23 +0300

    6.3 added JCommander

--- addressbook-web-tests/src/test/java/qa/pkg/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/qa/pkg/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package qa.pkg.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import qa.pkg.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,16 +13,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
 
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+    }
+    generator.run();
+
   }
 
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
       groups.add(new GroupData().withGroupName(String.format("Name %s", i))
@@ -28,7 +45,7 @@ public class GroupDataGenerator {
     return groups;
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void save(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
       writer.write(String.format("%s;%s;%s\n", group.getGroupName(), group.getHeader(), group.getFooter()));
