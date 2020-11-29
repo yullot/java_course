fa1c2e38c9e7c44968c09d186b47a03f364861e5
yullot <yulok1102@mail.ru>
2020-11-29 23:58:29 +0300

    14. added System.properties, cerated local.properties file

--- addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/ApplicationManager.java
@@ -5,7 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
   String browser;
 
@@ -15,22 +21,25 @@ public class ApplicationManager {
 
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
   }
 
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
     }
     //wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     SessionHelper sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
--- addressbook-web-tests/src/test/java/qa/pkg/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/qa/pkg/addressbook/tests/TestBase.java
@@ -1,18 +1,19 @@
 package qa.pkg.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import qa.pkg.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
-  public void setUp() {
+  public void setUp() throws IOException {
     app.init();
   }
 
