26ef8a2c5d2d5cdfb015fd271abe23a1743eb775
yullot <yulok1102@mail.ru>
2020-12-29 21:28:01 +0300

    10.2 Creaet file remote.properties for remote launch of addressbook

--- addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/ApplicationManager.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -30,11 +33,18 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     dbHelper = new DBHelper();
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      }
+    } else{
+      DesiredCapabilities capabilities=new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd=new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
     }
+
     //wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
