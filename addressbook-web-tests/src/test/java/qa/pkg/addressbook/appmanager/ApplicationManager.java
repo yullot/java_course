552d2df07b0c1462b29ac38f6668ef4cd1c9d657
yullot <yulok1102@mail.ru>
2020-12-30 01:37:16 +0300

    try

--- addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/qa/pkg/addressbook/appmanager/ApplicationManager.java
@@ -43,7 +43,7 @@ public class ApplicationManager {
     } else{
       DesiredCapabilities capabilities=new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform","win10")));
+     capabilities.setPlatform(Platform.fromString(System.getProperty("platform","win10")));
       wd=new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
     }
 
